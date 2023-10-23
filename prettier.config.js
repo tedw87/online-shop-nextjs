1ee8e0437f432d574e8a65827f96ac8227f67b39
Teodor Voicu <teodorvoicu87@gmail.com>
2023-10-23 20:05:53 +0300

    fix prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
+const config = {
+	plugins: ['prettier-plugin-tailwindcss'],
+	printWidth: 100,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'all',
+	bracketSameLine: true,
 };
+
+module.exports = config;
