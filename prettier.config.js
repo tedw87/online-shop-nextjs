474027c1fad77f7e40d1543000b56fd370984066
Teodor Voicu <teodorvoicu87@gmail.com>
2023-10-23 23:32:19 +0300

    add page

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,8 @@
 const config = {
-	plugins: ['prettier-plugin-tailwindcss'],
-	printWidth: 100,
-	tabWidth: 2,
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	jsxSingleQuote: true,
-	trailingComma: 'all',
-	bracketSameLine: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+  // printWidth: 100,
+  // tabWidth: 2,
+  // useTabs: true,
 };
 
 module.exports = config;
