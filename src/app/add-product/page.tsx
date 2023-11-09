e451eef0e62506b98029f9695cf4d08612628abf
Teodor Voicu <teodorvoicu87@gmail.com>
2023-11-09 14:47:31 +0200

    make add product form

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,12 +1,35 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
 export const metadata = {
   title: "Add Product - TheOShop",
 };
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
 
 export default function AddProductPage() {
   return (
     <div>
       <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form>
+      <form action={addProduct}>
         <input
           required
           name="name"
@@ -29,13 +52,11 @@ export default function AddProductPage() {
         <input
           required
           name="price"
-          placeholder="Name"
+          placeholder="Price"
           type="number"
           className="input-bordered input mb-3 w-full"
         />
-        <button className="btn-primary btn-block btn" type="submit">
-          Add Product
-        </button>
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
       </form>
     </div>
   );
